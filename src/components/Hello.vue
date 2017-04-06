b9a0fc491d356ad8022875bf50fb39a084a02d27
luch <121541855@qq.com>
2017-04-06 16:36:10 +0800

    vue项目环境搭建

---
+++ src/components/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <div class="div1">
+      div1
+    </div>
+  </div>
+</template>
+<script>
+import '../assets/css/global.css'
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg:'dsadsa'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped="" type="text/css">
+
+</style>
